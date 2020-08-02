36c45f231130757c4ae98311f178b9489a609f73
Denys <gryshko_denys@gmail.com>
2020-08-02 18:48:10 +0300

    deleted comments

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.css';
 import { UsersList } from './components/UsersList/UsersList';
 
-// const Months = ['January', 'February', 'March', 'April', 'May',
-//   'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 function App() {
   return (
     <UsersList />
