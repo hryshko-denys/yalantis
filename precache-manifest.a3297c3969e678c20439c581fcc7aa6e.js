f8854a0dd84e983a12cd3143ea229b76dbbecb43
Denys <gryshko_denys@gmail.com>
2020-08-02 16:51:58 +0300

    Updates

---
+++ precache-manifest.a3297c3969e678c20439c581fcc7aa6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d1e92aafd13043dd3871a2659627e5",
+    "url": "/yalantis/index.html"
+  },
+  {
+    "revision": "8af7212456eae1999c4c",
+    "url": "/yalantis/static/css/main.1f5c8521.chunk.css"
+  },
+  {
+    "revision": "3b3902b8700266088ae0",
+    "url": "/yalantis/static/js/2.c68b4eae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/yalantis/static/js/2.c68b4eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af7212456eae1999c4c",
+    "url": "/yalantis/static/js/main.5a7fae31.chunk.js"
+  },
+  {
+    "revision": "3e4add2e14b9c75276fc",
+    "url": "/yalantis/static/js/runtime-main.c97a4e3a.js"
+  }
+]);
